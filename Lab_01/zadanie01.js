0ed1777786b3cd301296a15684cb882632cf081d
Katarzyna Sosnowska <katarzyna.k.sosnowska@student.uj.edu.pl>
2015-10-09 01:00:43 +0200

    Położono podwaliny pod zadanie 1.

---
+++ Lab_01/zadanie01.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Louve on 09.10.2015.
+ */
+
+// funkcja rysujaca slonia (a przynajmniej cos na jego obraz)
+function drawElephant(){
+    var canvas = document.getElementById("MyFirstCanvas");  // tworzymy zmienna dla naszego canvas
+    var ctx = canvas.getContext("2d");                      // bierzemy kontekst naszego canvas
+
+    // rysowanie poszczegolnych elementow naszego slonia:
+
+    // ...algorytm w fazie projektowania
+}
