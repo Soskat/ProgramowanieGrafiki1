884dcb8714c6c798653080cd2fc8b5e6ccd53f23
Katarzyna Sosnowska <katarzyna.k.sosnowska@student.uj.edu.pl>
2015-10-14 00:24:57 +0200

    Dodanie do pliku figureLin.js nowych funkcji.

--- Lab_01/figuresLib.js
+++ Lab_01/figuresLib.js
@@ -37,3 +37,55 @@ function drawArc(){
     ctx.arc(50,50,30,0,30,1);
     ctx.fill();
 }
+
+
+// rysuje figure
+function drawFigure(){
+    var canvas = document.getElementById("MyFirstCanvas");  // tworzymy zmienna dla naszego canvas
+    var ctx = canvas.getContext("2d");                      // bierzemy kontekst naszego canvas
+    ctx.fillStyle = "rgba(0,255,0,1.0)";                    // okreslamy styl wypelnienia
+    ctx.lineWidth = 1;                                      // okreslamy grubosc linii konturu
+    ctx.strokeStyle = "#000000";                            // okreslamy styl konturu
+
+    //polygon(ctx, 100, 100, 50, 8);          // rysuje wielokat foremny
+    doodle(ctx, 100, 100, 100, 5, 4);        // rysuje gwiazde na planie wielokata foremnego
+}
+
+// rysuje wielokat foremny o n-katach
+function polygon(ctx, x, y, r, n){
+    ctx.beginPath();
+    ctx.moveTo(x + r * Math.cos(0), y + r * Math.sin(0));
+    for(var i = 1; i <= n; i += 1){
+        ctx.lineTo(
+            x + r * Math.cos(i * 2 * Math.PI / n),
+            y + r * Math.sin(i * 2 * Math.PI / n)
+        );
+    }
+    ctx.closePath();
+    ctx.fill();
+    ctx.stroke();
+}
+
+// rysuje gwiazdki
+function doodle(ctx, x, y, r, n, k){
+    var v = {};
+    // tworze liste wierzcholkow wielokata foremnego
+    for(var i = 1; i <= n; i += 1){
+        var p = {};
+        p.x = x + r * Math.cos(i * 2 * Math.PI / n);
+        p.y = y + r * Math.sin(i * 2 * Math.PI / n);
+        v[i] = p;
+    }
+
+    ctx.beginPath();
+    ctx.moveTo((v[0]).x, (v[0]).y);     // PROBLEM - jak odzyskac informacje o wspolrzednych punktu
+    var i = 1;
+    while((i*k - 1)%n != 0){
+        var j = (i*k - 1)%n;
+        ctx.lineTo((v[j]).x, (v[j]).y); // PROBLEM - jak powyzej
+        i += 1;
+    }
+    ctx.closePath();
+    ctx.stroke();
+    ctx.fill();
+}
