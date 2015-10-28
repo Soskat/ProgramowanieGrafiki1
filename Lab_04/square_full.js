a9ee2efe66bc735aee2c05f01fa16c079925f4ac
Katarzyna Sosnowska <katarzyna.k.sosnowska@student.uj.edu.pl>
2015-10-28 18:54:18 +0100

    Rysuje kształt kwadratu.

---
+++ Lab_04/square_full.js
@@ -0,0 +1,73 @@
+/**
+ * Created by Louve on 2015-10-28.
+ */
+
+var VSHADER_SOURCE =
+    'attribute vec3 position;\n' +
+    'void main(void){\n' +
+    '   gl_Position = vec4(position, 1.0);\n' +
+    '   gl_PointSize = 10.0;\n' +
+    '}\n';
+
+var FSHADER_SOURCE =
+    'precision mediump float;\n' +
+    'void main(void){\n' +
+    '   gl_FragColor = vec4(0.2, 0.7, 1.0, 1.0);\n' +
+    '}\n';
+
+
+function drawStuff(){
+    var canvas = document.getElementById("MyFirstCanvas");
+    var gl = canvas.getContext("webgl");
+
+    if(!gl){
+        console.log("Buka! :<");
+        return;
+    }
+
+    gl.viewportWidth = canvas.width;
+    gl.viewportHeight = canvas.height;
+
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.enable(gl.DEPTH_TEST);
+    gl.clearDepth(1.0);
+    gl.viewport(0, 0, gl.viewportWidth, gl.viewportHeight);
+
+    var pixelShader = gl.createShader(gl.FRAGMENT_SHADER);
+    gl.shaderSource(pixelShader, FSHADER_SOURCE);
+    gl.compileShader(pixelShader);
+
+    var vertexShader = gl.createShader(gl.VERTEX_SHADER);
+    gl.shaderSource(vertexShader, VSHADER_SOURCE);
+    gl.compileShader(vertexShader);
+
+    var program = gl.createProgram();
+
+    gl.attachShader(program, vertexShader);
+    gl.attachShader(program, pixelShader);
+    gl.linkProgram(program);
+
+    gl.useProgram(program);
+    gl.program = program;
+
+    program.position = gl.getAttribLocation(program, "position");
+    gl.enableVertexAttribArray(program.position);
+
+    var pointsBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, pointsBuffer);
+
+    var vertices = [
+        -0.5, 0.5,
+        0.5, 0.5,
+        0.5, -0.5,
+        -0.5, -0.5,
+        -0.5, 0.5
+    ];
+
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, pointsBuffer);
+    gl.vertexAttribPointer(program.position, 2, gl.FLOAT, false, 0, 0);
+    gl.drawArrays(gl.TRIANGLE_STRIP, 0, 5);
+}
