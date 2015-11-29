afc67fc8e124b9777ed20548391181eeb57f3b41
Katarzyna Sosnowska <katarzyna.k.sosnowska@student.uj.edu.pl>
2015-11-29 23:27:04 +0100

    Animacja 2d kwadratu (rotacja + translacja) - podejście 2..

--- Lab_06/animation2d_square.js
+++ Lab_06/animation2d_square.js
@@ -16,8 +16,13 @@ var FSHADER_SOURCE =
     '}\n';
 
 
+var newMatrix = new Float32Array(16);   // zmienna globalna
+var transMatrix = new Float32Array(16); // zmienna globalna
 var g_last = Date.now();
-function animate(angle){
+var Tx = 0.0, Ty = 0.0, Tz = 0.0;
+
+// Animacja kata obrotu:
+function animateAngle(angle){
     var ANGLE_STEP = 45.0;
     var now = Date.now();
     var elapsed = now - g_last; // milisec
@@ -26,13 +31,22 @@ function animate(angle){
     return newAngle %= 360;
 }
 
-var newMatrix = new Float32Array(16);   // zmienna globalna
+// Animacja polozenia wierzcholkow:
+function animatePosition(){
+    var STEP = 0.2;
+    var now = Date.now();
+    var elapsed = now - g_last; // milisec
+    g_last = now;
+    Tx = Tx + (STEP * elapsed) / 1000.0;
+    Ty = Ty + (STEP * elapsed) / 1000.0;
+    Tz = Tz + (STEP * elapsed) / 1000.0;
+}
 
-function setNewRotateMatrix(angle, x, y, z){
+// Ustawianie macierzy rotacji:
+function setNewRotateMatrix(angle){
     var radian = Math.PI * angle / 180.0;   // degr to radians
     var cosB = Math.cos(radian);
     var sinB = Math.sin(radian);
-
     // Elementy macierzy numerowane sa kolumnami!
     newMatrix[0] = cosB;
     newMatrix[1] = sinB;
@@ -40,18 +54,36 @@ function setNewRotateMatrix(angle, x, y, z){
     newMatrix[5] = cosB;
     newMatrix[10] = 1.0;
     newMatrix[15] = 1.0;
-
-    //console.log(newMatrix);
 }
 
-function draw(gl, n, currentAngle, u_ModelMatrix){
-    setNewRotateMatrix(currentAngle, 0,0,0);
+// Ustawianie macierzy translacji:
+function setNewTranslateMatrix(Tx, Ty, Tz){
+    transMatrix[0] = 1.0;
+    transMatrix[5] = 1.0;
+    transMatrix[10] = 1.0;
+    transMatrix[12] = Tx;
+    transMatrix[13] = Ty;
+    transMatrix[14] = Tz;
+    transMatrix[15] = 1.0;
+}
+
+// Rysowanie rotacji (aplikowanie macierzy rotacji):
+function drawRotation(gl, n, currentAngle, u_ModelMatrix){
+    setNewRotateMatrix(currentAngle);
     gl.uniformMatrix4fv(u_ModelMatrix, false, newMatrix);
 
     gl.clear(gl.COLOR_BUFFER_BIT);
     gl.drawArrays(gl.TRIANGLES, 0, n);
 }
 
+// Rysowanie translacji (aplikowanie macierzy translacji):
+function drawTranslation(gl, n, Tx, Ty, Tz, u_ModelMatrix){
+    setNewTranslateMatrix(Tx, Ty, Tz);
+    gl.uniformMatrix4fv(u_ModelMatrix, false, transMatrix);
+
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    gl.drawArrays(gl.TRIANGLES, 0, n);
+}
 
 function drawStuff(){
     var currentAngle = 0.0;
@@ -108,22 +140,22 @@ function drawStuff(){
 
     var tMatrix = gl.getUniformLocation(gl.program, 'tmatrix');
 
-    //gl.uniformMatrix4fv(tMatrix, false, transMatrix);
-
+    // === Tworzenie buforu z wspolrzednymi punktow figury ===
     var pointsBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, pointsBuffer);
 
-
     var vertices = new Float32Array([
-        0.0, 0.5, 0.0,
-        -0.5, -0.5, 0.0,
-        0.5, -0.5, 0.0
+        0.2, 0.2,
+        -0.2, 0.2,
+        0.2, -0.2,
+        -0.2, -0.2,
+        -0.2, 0.2,
+        0.2, -0.2
     ]);
 
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
-    pointsBuffer.itemSize = 3;
-    pointsBuffer.numItems = 3;
-
+    pointsBuffer.itemSize = 2;
+    pointsBuffer.numItems = 6;
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
     gl.bindBuffer(gl.ARRAY_BUFFER, pointsBuffer);
@@ -131,13 +163,17 @@ function drawStuff(){
 
     gl.uniformMatrix4fv(tMatrix, false, newMatrix);
 
-    gl.drawArrays(gl.TRIANGLES, 0, pointsBuffer.numItems);
+    gl.drawArrays(gl.TRIANGLE_STRIP, 0, pointsBuffer.numItems);
 
 
+    // === Funkcja animujaca figure ===
     var tick = function(){
-        currentAngle = animate(currentAngle);   // Update the rotation
-        console.log(currentAngle);
-        draw(gl, pointsBuffer.numItems, currentAngle, tMatrix);
+        // rotacja:
+        currentAngle = animateAngle(currentAngle);   // Update the rotation
+        drawRotation(gl, pointsBuffer.numItems, currentAngle, tMatrix);
+        // translacja:
+        //animatePosition();
+        //drawTranslation(gl, pointsBuffer.numItems, Tx, Ty, Tz, tMatrix);
         requestAnimationFrame(tick);    // request that the browser calls tick
     };
 
