9bcc1b8b2320e00445aa5fb819d1beac1aab4228
Katarzyna Sosnowska <katarzyna.k.sosnowska@student.uj.edu.pl>
2015-12-08 14:06:30 +0100

    Zadanie 3. - trzy punkty różnej wielkości w różnych kolorach.

--- Lab_07/zadanie03.js
+++ Lab_07/zadanie03.js
@@ -4,11 +4,12 @@
 
 var VSHADER_SOURCE =
     'attribute vec4 position;\n'+
+    'attribute float pointSize;\n'+
     'attribute vec4 a_color;\n'+
     'varying vec4 v_color;\n'+
     'void main() {\n' +
     '   gl_Position = position;\n' +
-    '   gl_PointSize = 10.0;\n' +
+    '   gl_PointSize = pointSize;\n' +
     '   v_color = a_color;\n' +
     '}\n';
 
@@ -19,13 +20,12 @@ var FSHADER_SOURCE =
     '   gl_FragColor = v_color;\n' + //kolor punktu
     '}\n';
 
-function drawStuff() {
 
+// Rysuje rzeczy w Canvasie:
+function drawStuff() {
     var canvas = document.getElementById('MyFirstCanvas');
     var gl = canvas.getContext("webgl");
     console.log(gl);
-
-
     if (!gl) {
         console.log('webGl nie bangla');
         return;
@@ -53,39 +53,37 @@ function drawStuff() {
 
 
     var n = 3;
+    // macierz wspolrzednych punktow, ich kolorow oraz rozmiarow
     var colouredVertices = new Float32Array
     (
-        [
-            0.0, 0.5, 1.0, 0.0, 0.0,
-            -0.5, -0.5, 0.0, 1.0, 0.0,
-            0.5, -0.5, 0.0, 0.0, 1.0
+        [   // wspol.:    // RGB:       // rozm.:
+             0.0,  0.5,   1.0, 0.0, 0.0,   10.0,
+            -0.5, -0.5,   0.0, 1.0, 0.0,   20.0,
+             0.5, -0.5,   0.0, 0.0, 1.0,   30.0
         ]
     );
 
-
-
-
-    var colouredVertexBuffer = gl.createBuffer();
+    var colouredVertexBuffer = gl.createBuffer();                   // bufor kolorowanych wierzcholkow
     gl.bindBuffer(gl.ARRAY_BUFFER, colouredVertexBuffer);
     gl.bufferData(gl.ARRAY_BUFFER, colouredVertices, gl.STATIC_DRAW);
 
     var position = gl.getAttribLocation(gl.program, 'position');
     var color = gl.getAttribLocation(gl.program, 'a_color');
+    var pointSize = gl.getAttribLocation(gl.program, 'pointSize');
 
-    var FSIZE = colouredVertices.BYTES_PER_ELEMENT;
+    var FSIZE = colouredVertices.BYTES_PER_ELEMENT;                 // rozmiar pojedynczego elementu
 
-    gl.vertexAttribPointer(position, 2, gl.FLOAT, false, FSIZE * 5, 0);
+    gl.vertexAttribPointer(position, 2, gl.FLOAT, false, FSIZE * 6, 0);
     gl.enableVertexAttribArray(position);
 
-    gl.vertexAttribPointer(color, 3, gl.FLOAT, false, FSIZE * 5, FSIZE * 2);
+    gl.vertexAttribPointer(color, 3, gl.FLOAT, false, FSIZE * 6, FSIZE * 2);
     gl.enableVertexAttribArray(color);
 
+    gl.vertexAttribPointer(pointSize, 1, gl.FLOAT, false, FSIZE * 6, FSIZE * 5);
+    gl.enableVertexAttribArray(pointSize);
+
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
-    gl.clear(gl.COLOR_BUFFER_BIT|gl.DEPTH_BUFFER_BIT);
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-    gl.drawArrays(gl.POINTS, 0, 3);
-
-
-
+    gl.drawArrays(gl.POINTS, 0, n);
 }
-
