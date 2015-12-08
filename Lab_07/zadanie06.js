05a8b42d6a9fa6bf83223ec4c7d28b0f8e33f43d
Katarzyna Sosnowska <katarzyna.k.sosnowska@student.uj.edu.pl>
2015-12-08 14:46:06 +0100

    Zadanie 6. - wgrywanie tekstury.

--- Lab_07/zadanie06.js
+++ Lab_07/zadanie06.js
@@ -13,19 +13,17 @@ var VSHADER_SOURCE =
 
 var FSHADER_SOURCE =
     'precision mediump float;\n' +
-    'uniform sample2D uSampler;\n' +
+    'uniform sampler2D uSampler;\n' +
     'varying vec2 vTexCoord;\n'+
     'void main(){\n' +
-    '   gl_FragColor = texture2D(uSampler, vTexCoord);\n' + //kolor punktu
+    '   gl_FragColor = texture2D(uSampler, vTexCoord);\n' + //tekstura
     '}\n';
 
+// Rysuje rzeczy w Canvasie:
 function drawStuff() {
-
     var canvas = document.getElementById('MyFirstCanvas');
     var gl = canvas.getContext("webgl");
     console.log(gl);
-
-
     if (!gl) {
         console.log('webGl nie bangla');
         return;
@@ -48,57 +46,53 @@ function drawStuff() {
     gl.attachShader(program, pixelShader);
     gl.linkProgram(program);
 
+    console.log(vertexShader);
+
     gl.useProgram(program);
     gl.program = program;
 
 
-
-
-
-    var textureBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, textureBuffer);
-    var vertices = new Float32Array
+    var n = 4;
+    var vertices = new Float32Array     // macierz wspolrzednych punktow i tekstur
     (
-        [
-            0.5, 0.5, 0.0, 0.5,
-            -0.5, 0.5, 0.0, 0.0,
-            0.5, -0.5, 0.5, 0.5,
-            -0.5, -0.5, 0.5, 0.0
+        [   // wspol.:   //wsp. tex.:
+             0.5,  0.5,    0.0, 0.5,
+            -0.5,  0.5,    0.0, 0.0,
+             0.5, -0.5,    0.5, 0.5,
+            -0.5, -0.5,    0.5, 0.0
         ]
     );
+
+    var textureBuffer = gl.createBuffer();          // bufor tekstury
+    gl.bindBuffer(gl.ARRAY_BUFFER, textureBuffer);
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
 
     var FSIZE = vertices.BYTES_PER_ELEMENT;
-    var n = 4;
 
     var vertexTexCoordBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, vertexTexCoordBuffer);
     gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
 
-    var a_Position = gl.getAttribLocation(gl.program, 'position');
-    gl.vertexAttribPointer(a_Position, 2, gl.float, false, FSIZE * 4, 0);
-    gl.enableVertexAttribArray(a_Position);
+    var a_position = gl.getAttribLocation(gl.program, 'position');
+    gl.vertexAttribPointer(a_position, 2, gl.FLOAT, false, FSIZE * 4, 0);
+    gl.enableVertexAttribArray(a_position);
 
     var a_TexCoord = gl.getAttribLocation(gl.program, 'aTexCoord');
-    gl.vertexAttribPointer(a_TexCoord, 2, gl.float, false, FSIZE * 4, FSIZE * 2);
+    gl.vertexAttribPointer(a_TexCoord, 2, gl.FLOAT, false, FSIZE * 4, FSIZE * 2);
     gl.enableVertexAttribArray(a_TexCoord);
 
-    program.position = gl.getAttribLocation(program, 'position');
-    gl.enableVertexAttribArray(program.position);
 
-
+    // tworzymy teksture:
     var texture = gl.createTexture();
-
     var u_Sampler = gl.getUniformLocation(gl.program, 'uSampler');
     var img = new Image();
-    img.src = "http://localhost:63342/stones.png";
+    img.src = "http://localhost:63342/ProgramowanieGrafiki1/Lab_07/lava.jpg";
     img.onload = function(){
-       gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, 1);
+        gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, 1);
 
         gl.activeTexture(gl.TEXTURE0);
         gl.bindTexture(gl.TEXTURE_2D, texture);
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
-
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
         gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, gl. RGB, gl.UNSIGNED_BYTE, img);
@@ -109,4 +103,3 @@ function drawStuff() {
         gl.drawArrays(gl.TRIANGLE_STRIP, 0, n);
     };
 }
-
