8d7db7b082bd644d7ee63f8b2d74d1d70145eb3e
Katarzyna Sosnowska <katarzyna.k.sosnowska@student.uj.edu.pl>
2015-12-09 18:55:07 +0100

    Zad 4 - robocze

--- Lab_08/zad04.js
+++ Lab_08/zad04.js
@@ -153,6 +153,8 @@ function drawStuff() {
     gl.enableVertexAttribArray(color);
 
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.enable(gl.DEPTH_TEST);
+    gl.clearDepth(1.0);
     gl.clear(gl.COLOR_BUFFER_BIT|gl.DEPTH_BUFFER_BIT);
 
     gl.drawArrays(gl.TRIANGLES, 0, n);
