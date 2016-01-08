b5d50ac9e4ad06771d244398b859059507ddc645
Katarzyna Sosnowska <katarzyna.k.sosnowska@student.uj.edu.pl>
2016-01-08 17:04:29 +0100

    Teksturowanie - wersja alpha.

--- Zaliczeniowy/zaliczenie.js
+++ Zaliczeniowy/zaliczenie.js
@@ -7,21 +7,22 @@
 
 var VSHADER_SOURCE =
     'attribute vec4 position;\n'+
-    'attribute vec4 a_color;\n'+
-    'varying vec4 v_color;\n'+
     'uniform mat4 u_ViewMatrix;\n'+
     'uniform mat4 rmatrix;\n'+
     'uniform mat4 tmatrix;\n'+
+    'attribute vec2 aTexCoord;\n'+
+    'varying vec2 vTexCoord;\n'+
     'void main() {\n' +
     '   gl_Position = u_ViewMatrix * tmatrix * rmatrix * position;\n' +
-    '   v_color = a_color;\n' +
+    '   vTexCoord = aTexCoord;\n' +
     '}\n';
 
 var FSHADER_SOURCE =
     'precision mediump float;\n' +
-    'varying vec4 v_color;\n'+
+    'uniform sampler2D uSampler;\n' +
+    'varying vec2 vTexCoord;\n'+
     'void main(){\n' +
-    '   gl_FragColor = v_color;\n' +  //kolor punktu
+    '   gl_FragColor = texture2D(uSampler, vTexCoord);\n' + //tekstura
     '}\n';
 
 // == Funkcje pomocnicze ==========================================================================
@@ -104,7 +105,6 @@ function keydown(ev, gl, n, u_ViewMatrix){
 }
 
 
-
 var rotMatrixCube = new Float32Array(16);   // macierz rotacji szescianu
 var rotMatrixSphere = new Float32Array(16); // macierz rotacji sfery
 var identity = new Float32Array([1,0,0,0, 0,1,0,0, 0,0,1,0, 0,0,0,1]);
@@ -154,6 +154,7 @@ function setNewSphereTranslationMatrix(angle, r){
     transMatrix[14] = r * cosB;     // vz
 }
 
+
 var g_last = Date.now();
 var currentAngle = 0.0;
 
@@ -188,7 +189,6 @@ function animate(gl, u_ViewMatrix, rMatrix, tMatrix, r, floorN, cubeN, sphereN){
     // ustawiam wyjsciowa rotacje i rysuje sfere:
     gl.uniformMatrix4fv(tMatrix, false, transMatrix);
     gl.uniformMatrix4fv(rMatrix, false, rotMatrixSphere);
-    //gl.uniformMatrix4fv(rMatrix, false, identity);
     gl.drawArrays(gl.TRIANGLES, floorN + cubeN, sphereN);
 }
 
@@ -236,116 +236,124 @@ function drawStuff() {
     gl.clear(gl.COLOR_BUFFER_BIT|gl.DEPTH_BUFFER_BIT);
 
 
-    var coloredVertices = new Float32Array
+    var texturedVertices = new Float32Array
     (
-        [  // wspolrz.:   // RGB:
+        [  // wspolrz.:    // wspolrz. tekstury:
 
             // podloze:
-            -0.5, -0.5, -0.5,  0.5, 0.5, 0.5,
-            -0.5, -0.5,  0.5,  0.5, 0.5, 0.5,
-             0.5, -0.5, -0.5,  0.5, 0.5, 0.5,
-             0.5, -0.5, -0.5,  0.5, 0.5, 0.5,
-            -0.5, -0.5,  0.5,  0.5, 0.5, 0.5,
-             0.5, -0.5,  0.5,  0.5, 0.5, 0.5,
+            -0.5, -0.5, -0.5,   0.25, 0.25,
+            -0.5, -0.5,  0.5,   0.25, 0.5,
+             0.5, -0.5, -0.5,   0.5,  0.25,
+             0.5, -0.5, -0.5,   0.5,  0.25,
+            -0.5, -0.5,  0.5,   0.25, 0.5,
+             0.5, -0.5,  0.5,   0.5,  0.5,
 
             // szescian:
-            -0.1, -0.1,  0.1,   0.1, 0.8, 0.2,
-             0.1, -0.1,  0.1,   0.1, 0.8, 0.2,
-            -0.1, -0.1, -0.1,   0.1, 0.8, 0.2,
-            -0.1, -0.1, -0.1,   0.1, 0.8, 0.2,
-             0.1, -0.1,  0.1,   0.1, 0.8, 0.2,
-             0.1, -0.1, -0.1,   0.1, 0.8, 0.2,
+            -0.1, -0.1,  0.1,   0.5,  0.5,
+             0.1, -0.1,  0.1,   1.0,  0.5,
+            -0.1, -0.1, -0.1,   0.5,  0.75,
+            -0.1, -0.1, -0.1,   0.5,  0.75,
+             0.1, -0.1,  0.1,   1.0,  0.5,
+             0.1, -0.1, -0.1,   1.0,  0.75,
 
-            -0.1, -0.1, -0.1,   0.4, 0.3, 0.2,
-             0.1, -0.1, -0.1,   0.4, 0.3, 0.2,
-            -0.1,  0.1, -0.1,   0.4, 0.3, 0.2,
-            -0.1,  0.1, -0.1,   0.4, 0.3, 0.2,
-             0.1, -0.1, -0.1,   0.4, 0.3, 0.2,
-             0.1,  0.1, -0.1,   0.4, 0.3, 0.2,
+            -0.1, -0.1, -0.1,   0.5,  0.0,
+             0.1, -0.1, -0.1,   1.0,  0.0,
+            -0.1,  0.1, -0.1,   0.5,  0.25,
+            -0.1,  0.1, -0.1,   0.5,  0.25,
+             0.1, -0.1, -0.1,   1.0,  0.0,
+             0.1,  0.1, -0.1,   1.0,  0.25,
 
-            -0.1, -0.1,  0.1,   0.3, 0.5, 0.2,
-            -0.1, -0.1, -0.1,   0.3, 0.5, 0.2,
-            -0.1,  0.1,  0.1,   0.3, 0.5, 0.2,
-            -0.1,  0.1,  0.1,   0.3, 0.5, 0.2,
-            -0.1, -0.1, -0.1,   0.3, 0.5, 0.2,
-            -0.1,  0.1, -0.1,   0.3, 0.5, 0.2,
+            -0.1, -0.1,  0.1,   0.0,  0.5,
+            -0.1, -0.1, -0.1,   0.0,  0.25,
+            -0.1,  0.1,  0.1,   0.5,  0.5,
+            -0.1,  0.1,  0.1,   0.5,  0.5,
+            -0.1, -0.1, -0.1,   0.0,  0.25,
+            -0.1,  0.1, -0.1,   0.5,  0.25,
 
-            -0.1, -0.1,  0.1,   0.4, 0.3, 0.2,
-             0.1, -0.1,  0.1,   0.4, 0.3, 0.2,
-            -0.1,  0.1,  0.1,   0.4, 0.3, 0.2,
-            -0.1,  0.1,  0.1,   0.4, 0.3, 0.2,
-             0.1, -0.1,  0.1,   0.4, 0.3, 0.2,
-             0.1,  0.1,  0.1,   0.4, 0.3, 0.2,
+            -0.1, -0.1,  0.1,   0.0,  0.25,
+             0.1, -0.1,  0.1,   0.5,  0.25,
+            -0.1,  0.1,  0.1,   0.0,  0.0,
+            -0.1,  0.1,  0.1,   0.0,  0.0,
+             0.1, -0.1,  0.1,   0.5,  0.25,
+             0.1,  0.1,  0.1,   0.5,  0.0,
 
-             0.1, -0.1,  0.1,   0.3, 0.5, 0.2,
-             0.1,  0.1,  0.1,   0.3, 0.5, 0.2,
-             0.1, -0.1, -0.1,   0.3, 0.5, 0.2,
-             0.1, -0.1, -0.1,   0.3, 0.5, 0.2,
-             0.1,  0.1,  0.1,   0.3, 0.5, 0.2,
-             0.1,  0.1, -0.1,   0.3, 0.5, 0.2,
+             0.1, -0.1,  0.1,   1.0,  0.5,
+             0.1,  0.1,  0.1,   0.5,  0.5,
+             0.1, -0.1, -0.1,   1.0,  0.25,
+             0.1, -0.1, -0.1,   1.0,  0.25,
+             0.1,  0.1,  0.1,   0.5,  0.5,
+             0.1,  0.1, -0.1,   0.5,  0.25,
 
-             0.1,  0.1,  0.1,   0.1, 0.8, 0.2,
-             0.1,  0.1, -0.1,   0.1, 0.8, 0.2,
-            -0.1,  0.1,  0.1,   0.1, 0.8, 0.2,
-            -0.1,  0.1,  0.1,   0.1, 0.8, 0.2,
-             0.1,  0.1, -0.1,   0.1, 0.8, 0.2,
-            -0.1,  0.1, -0.1,   0.1, 0.8, 0.2,
+             0.1,  0.1,  0.1,   0.5,  0.75,
+             0.1,  0.1, -0.1,   0.5,  0.5,
+            -0.1,  0.1,  0.1,   0.0,  0.75,
+            -0.1,  0.1,  0.1,   0.0,  0.75,
+             0.1,  0.1, -0.1,   0.5,  0.5,
+            -0.1,  0.1, -0.1,   0.0,  0.5,
 
 
             // sfera:
-            -0.1, -0.1,  0.1,   0.2, 0.6, 0.1,
-             0.1, -0.1,  0.1,   0.2, 0.6, 0.1,
-            -0.1, -0.1, -0.1,   0.2, 0.6, 0.1,
-            -0.1, -0.1, -0.1,   0.2, 0.6, 0.1,
-             0.1, -0.1,  0.1,   0.2, 0.6, 0.1,
-             0.1, -0.1, -0.1,   0.2, 0.6, 0.1,
+            -0.1, -0.1,  0.1,   0.5,  0.5,
+             0.1, -0.1,  0.1,   1.0,  0.5,
+            -0.1, -0.1, -0.1,   0.5,  0.75,
+            -0.1, -0.1, -0.1,   0.5,  0.75,
+             0.1, -0.1,  0.1,   1.0,  0.5,
+             0.1, -0.1, -0.1,   1.0,  0.75,
 
-            -0.1, -0.1, -0.1,   0.4, 0.7, 0.3,
-             0.1, -0.1, -0.1,   0.4, 0.7, 0.3,
-            -0.1,  0.1, -0.1,   0.4, 0.7, 0.3,
-            -0.1,  0.1, -0.1,   0.4, 0.7, 0.3,
-             0.1, -0.1, -0.1,   0.4, 0.7, 0.3,
-             0.1,  0.1, -0.1,   0.4, 0.7, 0.3,
+            -0.1, -0.1, -0.1,   0.5,  0.0,
+             0.1, -0.1, -0.1,   1.0,  0.0,
+            -0.1,  0.1, -0.1,   0.5,  0.25,
+            -0.1,  0.1, -0.1,   0.5,  0.25,
+             0.1, -0.1, -0.1,   1.0,  0.0,
+             0.1,  0.1, -0.1,   1.0,  0.25,
 
-            -0.1, -0.1,  0.1,   0.2, 0.2, 0.6,
-            -0.1, -0.1, -0.1,   0.2, 0.2, 0.6,
-            -0.1,  0.1,  0.1,   0.2, 0.2, 0.6,
-            -0.1,  0.1,  0.1,   0.2, 0.2, 0.6,
-            -0.1, -0.1, -0.1,   0.2, 0.2, 0.6,
-            -0.1,  0.1, -0.1,   0.2, 0.2, 0.6,
+            -0.1, -0.1,  0.1,   0.0,  0.5,
+            -0.1, -0.1, -0.1,   0.0,  0.25,
+            -0.1,  0.1,  0.1,   0.5,  0.5,
+            -0.1,  0.1,  0.1,   0.5,  0.5,
+            -0.1, -0.1, -0.1,   0.0,  0.25,
+            -0.1,  0.1, -0.1,   0.5,  0.25,
 
-            -0.1, -0.1,  0.1,   0.4, 0.7, 0.3,
-             0.1, -0.1,  0.1,   0.4, 0.7, 0.3,
-            -0.1,  0.1,  0.1,   0.4, 0.7, 0.3,
-            -0.1,  0.1,  0.1,   0.4, 0.7, 0.3,
-             0.1, -0.1,  0.1,   0.4, 0.7, 0.3,
-             0.1,  0.1,  0.1,   0.4, 0.7, 0.3,
+            -0.1, -0.1,  0.1,   0.0,  0.25,
+             0.1, -0.1,  0.1,   0.5,  0.25,
+            -0.1,  0.1,  0.1,   0.0,  0.0,
+            -0.1,  0.1,  0.1,   0.0,  0.0,
+             0.1, -0.1,  0.1,   0.5,  0.25,
+             0.1,  0.1,  0.1,   0.5,  0.0,
 
-             0.1, -0.1,  0.1,   0.2, 0.2, 0.6,
-             0.1,  0.1,  0.1,   0.2, 0.2, 0.6,
-             0.1, -0.1, -0.1,   0.2, 0.2, 0.6,
-             0.1, -0.1, -0.1,   0.2, 0.2, 0.6,
-             0.1,  0.1,  0.1,   0.2, 0.2, 0.6,
-             0.1,  0.1, -0.1,   0.2, 0.2, 0.6,
+             0.1, -0.1,  0.1,   1.0,  0.5,
+             0.1,  0.1,  0.1,   0.5,  0.5,
+             0.1, -0.1, -0.1,   1.0,  0.25,
+             0.1, -0.1, -0.1,   1.0,  0.25,
+             0.1,  0.1,  0.1,   0.5,  0.5,
+             0.1,  0.1, -0.1,   0.5,  0.25,
 
-             0.1,  0.1,  0.1,   0.2, 0.6, 0.1,
-             0.1,  0.1, -0.1,   0.2, 0.6, 0.1,
-            -0.1,  0.1,  0.1,   0.2, 0.6, 0.1,
-            -0.1,  0.1,  0.1,   0.2, 0.6, 0.1,
-             0.1,  0.1, -0.1,   0.2, 0.6, 0.1,
-            -0.1,  0.1, -0.1,   0.2, 0.6, 0.1
+             0.1,  0.1,  0.1,   0.5,  0.75,
+             0.1,  0.1, -0.1,   0.5,  0.5,
+            -0.1,  0.1,  0.1,   0.0,  0.75,
+            -0.1,  0.1,  0.1,   0.0,  0.75,
+             0.1,  0.1, -0.1,   0.5,  0.5,
+            -0.1,  0.1, -0.1,   0.0,  0.5
         ]
     );
-    var N = coloredVertices.length / 6;
+    var N = texturedVertices.length / 6;
     var floorN = 6, cubeN = 36, sphereN = 36;
 
+    var FSIZE = texturedVertices.BYTES_PER_ELEMENT;     // rozmiar pojedynczego elementu w buforze
 
 
-    var FSIZE = coloredVertices.BYTES_PER_ELEMENT;     // rozmiar pojedynczego elementu w buforze
+    // tworzenie bufora tekstury:
+    var textureBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, textureBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(texturedVertices), gl.STATIC_DRAW);
+
+    // tworzenie bufora punktow:
+    var vertexBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, texturedVertices, gl.STATIC_DRAW);
+
 
     // wyciaganie danych z shadera:
-    var position = gl.getAttribLocation(gl.program, 'position');
-    var color = gl.getAttribLocation(gl.program, 'a_color');
 
     var u_ViewMatrix = gl.getUniformLocation(gl.program, 'u_ViewMatrix');               // macierz perspektywy
     document.onkeydown = function(ev){ keydown(ev, gl, N, u_ViewMatrix, viewMatrix); }; // uruchamiamy obsluge klawiszy
@@ -358,21 +366,41 @@ function drawStuff() {
     var tMatrix = gl.getUniformLocation(gl.program, 'tmatrix');     // macierz translacji
     gl.uniformMatrix4fv(tMatrix, false, identity);
 
-    // tworzenie bufora punktow:
-    var floorVertexBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, floorVertexBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, coloredVertices, gl.STATIC_DRAW);
-
-    gl.vertexAttribPointer(position, 3, gl.FLOAT, false, FSIZE * 6, 0);
+    var position = gl.getAttribLocation(gl.program, 'position');
+    gl.vertexAttribPointer(position, 3, gl.FLOAT, false, FSIZE * 5, 0);
     gl.enableVertexAttribArray(position);
-    gl.vertexAttribPointer(color, 3, gl.FLOAT, false, FSIZE * 6, FSIZE * 3);
-    gl.enableVertexAttribArray(color);
+
+    var a_TextCoord = gl.getAttribLocation(gl.program, 'aTexCoord');
+    gl.vertexAttribPointer(a_TextCoord, 2, gl.FLOAT, false, FSIZE * 5, FSIZE * 3);
+    gl.enableVertexAttribArray(a_TextCoord);
+
+
+    // tworzymy teksture:
+    var texture = gl.createTexture();
+    var u_Sampler = gl.getUniformLocation(gl.program, 'uSampler');
+    var img = new Image();
+    img.src = "differentWalls.jpg";
+    img.onload = function(){
+        gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, 1);
+
+        gl.activeTexture(gl.TEXTURE0);
+        gl.bindTexture(gl.TEXTURE_2D, texture);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+        gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, gl. RGB, gl.UNSIGNED_BYTE, img);
+        gl.uniform1i(u_Sampler, 0);
+    };
+
+
+
+
 
     // rysowanie elementow sceny:
     gl.drawArrays(gl.TRIANGLES, 0, N);
 
 
-
+    // animowanie sceny:
     var tick = function(){
         animate(gl, u_ViewMatrix, rMatrix, tMatrix, 0.5, floorN, cubeN, sphereN);    // uruchamia animacje elementow sceny
         requestAnimationFrame(tick);                                            // request that the browser calls tick
