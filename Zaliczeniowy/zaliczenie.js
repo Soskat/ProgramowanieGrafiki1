4fe37d3f7748bd9db7170c6fc4b888a93fb8910f
Katarzyna Sosnowska <katarzyna.k.sosnowska@student.uj.edu.pl>
2016-01-08 18:43:00 +0100

    Teksturowanie - alpha c.d.

--- Zaliczeniowy/zaliczenie.js
+++ Zaliczeniowy/zaliczenie.js
@@ -198,18 +198,19 @@ function animate(gl, u_ViewMatrix, rMatrix, tMatrix, r, floorN, cubeN, sphereN){
 
 
 // Wczytywanie tekstury:
-function loadTextureSettings(gl, u_Sampler, texture, img, first_el, N){
+function loadTextureSettings(gl, gl_texture, u_Sampler, index, texture, img, first_el, N){
     gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, 1);
-    gl.activeTexture(gl.TEXTURE0);
+    //gl.activeTexture(gl.TEXTURE0);
+    gl.activeTexture(gl_texture);
     gl.bindTexture(gl.TEXTURE_2D, texture);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
     gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, gl. RGB, gl.UNSIGNED_BYTE, img);
-    gl.uniform1i(u_Sampler, 0);
+    gl.uniform1i(u_Sampler, index);
 
-    gl.clearColor(0,0,0,1);
-    gl.clear(gl.COLOR_BUFFER_BIT);
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT|gl.DEPTH_BUFFER_BIT);
     gl.drawArrays(gl.TRIANGLES, first_el, N);
 }
 
@@ -219,6 +220,7 @@ function loadTextureSettings(gl, u_Sampler, texture, img, first_el, N){
 
 // Rysuje rzeczy w Canvasie:
 function drawStuff() {
+    // /ladowanie shaderow do programu
     var canvas = document.getElementById('MyFirstCanvas');
     var gl = canvas.getContext("webgl");
     console.log(gl);
@@ -399,24 +401,24 @@ function drawStuff() {
     var floorTexture = gl.createTexture();
     var floorImg = new Image();
     floorImg.src = "basketStyle.jpg";
-    floorImg.onload = function(){ loadTextureSettings(gl, floor_u_Sampler, floorTexture, floorImg, 0, floorN); };
+    floorImg.onload = function(){ loadTextureSettings(gl, gl.TEXTURE0, floor_u_Sampler, 0, floorTexture, floorImg, 0, floorN); };
 
     var cube_u_Sampler = gl.getUniformLocation(gl.program, 'uSampler2');
     var cubeTexture = gl.createTexture();
     var cubeImg = new Image();
     cubeImg.src = "differentWalls.jpg";
-    cubeImg.onload = function(){ loadTextureSettings(gl, cube_u_Sampler, cubeTexture, cubeImg, floorN, cubeN); };
+    cubeImg.onload = function(){ loadTextureSettings(gl, gl.TEXTURE1, cube_u_Sampler, 1, cubeTexture, cubeImg, floorN, cubeN); };
 
     var sphere_u_Sampler = gl.getUniformLocation(gl.program, 'uSampler3');
     var sphereTexture = gl.createTexture();
     var sphereImg = new Image();
     sphereImg.src = "cracked.jpg";
-    sphereImg.onload = function(){ loadTextureSettings(gl, sphere_u_Sampler, sphereTexture, sphereImg, floorN + cubeN, sphereN); };
+    sphereImg.onload = function(){ loadTextureSettings(gl, gl.TEXTURE2, sphere_u_Sampler, 2, sphereTexture, sphereImg, floorN + cubeN, sphereN); };
 
 
     // animowanie sceny:
     var tick = function(){
-        //animate(gl, u_ViewMatrix, rMatrix, tMatrix, 0.5, floorN, cubeN, sphereN);    // uruchamia animacje elementow sceny
+        animate(gl, u_ViewMatrix, rMatrix, tMatrix, 0.5, floorN, cubeN, sphereN);    // uruchamia animacje elementow sceny
         requestAnimationFrame(tick);                                            // request that the browser calls tick
     };
 
